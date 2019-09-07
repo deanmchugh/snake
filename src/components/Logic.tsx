dbd101444a0265acaf0b085fed6a67f099c24236
Dean McHugh <dean.mchugh1988@gmail.com>
2019-09-07 22:45:10 +1000

    corrected location selection

--- src/components/Logic.tsx
+++ src/components/Logic.tsx
@@ -1,25 +1,26 @@
-let snake = [2525]
+let snake = ['2525']
 
-const coords = (position: number) => {
-    const pos = position.toString()
-    const xcood = parseInt(pos.slice(0,2))
-    const ycood = parseInt(pos.slice(2,4))
+const coords = (position: string) => {
+    const xcood = position.slice(0,2)
+    const ycood = position.slice(2,4)
     return {xcood, ycood}
 }
 
 const direction = (input: string) => {
-    let {xcood, ycood} = coords(snake[snake.length - 1])
-    if (input === 'w') xcood = xcood - 1; 
-    if (input === 's') xcood = xcood + 1;
-    if (input === 'a') ycood = ycood - 1;
-    if (input === 'd') ycood = ycood + 1;
-    snake.push(parseInt(xcood + '' + ycood))
+    const {xcood, ycood} = coords(snake[snake.length - 1])
+    let x = parseInt(xcood); let y = parseInt(ycood)
+    if (input === 'w') x = x - 1; 
+    if (input === 's') x = x + 1;
+    if (input === 'a') y = y - 1;
+    if (input === 'd') y = y + 1;
+    const xStr = x.toString().padStart(2, '0')
+    const yStr = y.toString().padStart(2, '0')
+    snake.push(xStr + yStr)
 }
 
 const Logic = (input: string) => {
     direction(input)
-    console.log(snake)
-    const square = document.getElementById(snake[snake.length - 1].toString())
+    const square: HTMLElement | null = document.getElementById(snake[snake.length - 1])
     square.style.background = 'gray'
 }
 
