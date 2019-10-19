67ccf9b720ab35ba0d2717c9c11d12f61108027e
Dean McHugh <dean.mchugh1988@gmail.com>
2019-10-19 10:37:03 +1000

    moving and food

--- src/components/Logic.tsx
+++ src/components/Logic.tsx
@@ -3,6 +3,8 @@ const MIN = 0
 const WIDTHMAX = BOARD.WIDTH - 1
 const HEIGHTMAX = BOARD.HEIGHT - 1
 let direction = ''
+let length = 5
+let FOOD = ''
 
 const coords = (position: string) => {
     const xcood = position.slice(0,2)
@@ -17,7 +19,29 @@ const getDirection = (input: string) => {
     if (input === 'd') direction = 'right';
 }
 
-const engine = () => {
+const setSquare = (square: string, color: string) => {
+    const selectSquare: HTMLElement | null = document.getElementById(square)
+    selectSquare.style.background = color
+}
+
+const setFood = () => {
+    if (!FOOD || SNAKE.includes(FOOD)) {
+        const x = Math.floor(Math.random() * WIDTHMAX).toString().padStart(2, '0')
+        const y = Math.floor(Math.random() * HEIGHTMAX).toString().padStart(2, '0')
+        FOOD = `${x}${y}`
+        setSquare(FOOD, 'red')
+        length = length + 5
+    }
+    setTimeout(drawSnake, 900)
+}
+
+const drawSnake = () => {
+    setSquare(SNAKE[0], 'blue')
+    setSquare(SNAKE[SNAKE.length - 1], 'gray')
+    setTimeout(drawSnake, 1000)
+}
+
+const createSnake = () => {
     const {xcood, ycood} = coords(SNAKE[SNAKE.length - 1])
     let x = parseInt(xcood); let y = parseInt(ycood)
     if (direction === 'up') x = x - 1; 
@@ -29,13 +53,14 @@ const engine = () => {
         const yStr = y.toString().padStart(2, '0')
         SNAKE.push(xStr + yStr)
     }
-    const square: HTMLElement | null = document.getElementById(SNAKE[SNAKE.length - 1])
-    square.style.background = 'gray'
-    setTimeout(engine, 300)
+    if (SNAKE.length === length) SNAKE.shift()
+    setTimeout(createSnake, 1000)
 }
 
 const Logic = (input: string) => {
-    engine()
+    createSnake()
+    drawSnake()
+    setFood()
     getDirection(input)
 }
 
