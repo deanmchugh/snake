9c993bfadd1bd96495214ce98b3db031a7937fb9
Dean McHugh <dean.mchugh1988@gmail.com>
2019-11-01 21:43:14 +1000

    same line off screen

--- src/components/Logic.tsx
+++ src/components/Logic.tsx
@@ -4,10 +4,10 @@ let length = 5
 let FOOD = 0
 
 const getDirection = (input: string) => {
-    if (input === 'w') direction = 'up'; 
-    if (input === 's') direction = 'down';
-    if (input === 'a') direction = 'left';
-    if (input === 'd') direction = 'right';
+    if (input === 'w') direction = 'up'
+    if (input === 's') direction = 'down'
+    if (input === 'a') direction = 'left'
+    if (input === 'd') direction = 'right'
 }
 
 const setSquare = (square: number, color: string) => {
@@ -32,10 +32,14 @@ const drawSnake = () => {
 
 const engine = () => {
     let snakeHead = SNAKE[SNAKE.length - 1]
-    if (direction === 'up') snakeHead = snakeHead - 50; 
-    else if (direction === 'down') snakeHead = snakeHead + 50;
-    else if (direction === 'left') snakeHead = snakeHead - 1;
-    else if (direction === 'right') snakeHead = snakeHead + 1;
+    if (direction === 'up') 1 < snakeHead && snakeHead < BOARD.WIDTH ? 
+        snakeHead = snakeHead + 2450 : snakeHead = snakeHead - 50
+    else if (direction === 'down') 2451 < snakeHead && snakeHead < 2500 ? 
+        snakeHead = snakeHead - 2450 : snakeHead = snakeHead + 50
+    else if (direction === 'left') snakeHead % 50 === 0 + 1 ? 
+        snakeHead = snakeHead + 49 : snakeHead = snakeHead - 1
+    else if (direction === 'right') snakeHead % 50 === 0 ? 
+        snakeHead = snakeHead - 49 : snakeHead = snakeHead + 1
     if (snakeHead < (BOARD.WIDTH * BOARD.HEIGHT)){
         SNAKE.push(snakeHead)
     }
